3fc678f2223062583fd3720d780512a8606eaaab
rshimoda <rshimoda@gmail.com>
2018-02-26 13:41:01 -0500

    Adding lab projects to git

---
+++ lab/GAME1012_Lab2_NakasakoRicardo/Assets/My Assets/Scripts/Rotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	[SerializeField] Vector3 rotationAxis;
+	[SerializeField] float maxRotationAngle;
+	[SerializeField] float minRotationAngle;
+	[SerializeField] bool clampRotation;
+	float currentAngle = 0;
+
+	// Use this for initialization
+	void Start () {
+		// this reset the angle to 0 - for the turret
+		transform.eulerAngles = rotationAxis * currentAngle; // or Vector3.zero
+	}
+	
+	// Update is called once per frame
+	/*
+	void Update () {
+		
+	}*/
+
+	public void Rotate(float rotationAngle){
+		float newAngle = rotationAngle + currentAngle;
+		if(!clampRotation || (newAngle >= minRotationAngle && newAngle <= maxRotationAngle))
+		{
+			transform.Rotate (rotationAxis * rotationAngle);
+			currentAngle = newAngle;
+		}
+	}
+}
---
+++ lab/GAME1012_Lab2_NakasakoRicardo/Assets/My Assets/Scripts/Turret.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	[SerializeField] float tankSpeed; // Tank movement speed - back and forth
+	[SerializeField] float tankRotSpeed; // Tank rotation speed - yes it can make curves
+
+	[SerializeField] float turretRotSpeed; // turrent rotation speed - targeting enemies
+	[SerializeField] Rotation turretRotation; // script to rotate the turret
+
+	[SerializeField] float barrelRotSpeed; // speed to rotate the barrel up and down
+	[SerializeField] Rotation barrelRotation; // script to rotate the barrel
+
+	[SerializeField] GameObject projectilePrefab; // Prefab to create the projectile
+	[SerializeField] Transform projectileSpawn; // Where the projectile is instantiated
+
+	// Update is called once per frame
+	void Update () {
+		MoveTank ();
+		RotateTurret ();
+		RotateBarrel ();
+		FireInput ();
+	}
+	void RotateTurret(){
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			turretRotation.Rotate (-turretRotSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			turretRotation.Rotate (turretRotSpeed * Time.deltaTime);
+		}
+	}
+	void RotateBarrel () {
+		if (Input.GetKey(KeyCode.UpArrow))
+			barrelRotation.Rotate(-barrelRotSpeed * Time.deltaTime);
+		if (Input.GetKey(KeyCode.DownArrow))
+			barrelRotation.Rotate(barrelRotSpeed * Time.deltaTime);
+	}
+
+	void FireInput ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			Instantiate(projectilePrefab, projectileSpawn.position, projectileSpawn.rotation);
+	} 
+	void MoveTank(){
+		// move tank
+		if(Input.GetKey(KeyCode.W)){
+			// Moves along z at speed per second
+			transform.Translate (0, 0, tankSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, 0, -tankSpeed * Time.deltaTime);
+		}
+
+		// rotate tank
+		if(Input.GetKey(KeyCode.A)){
+			// Rotate Left
+			transform.Rotate (0, -tankRotSpeed * Time.deltaTime,0);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			// Rotate Right
+			transform.Rotate (0, tankRotSpeed * Time.deltaTime,0);
+		}
+
+	}
+}

630471cdf340c4ee62dd02adfd93172fe3d7ccdd
Ricardo Shimoda <rshimoda@gmail.com>
2018-04-21 18:54:12 -0400

    all the changes

---
+++ assignments/GAME1012_A2_NakasakoRicardo/Assets/MyAssets/Scripts/DrillScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillScript : MonoBehaviour {
+	[SerializeField] AudioSource aud;
+	[SerializeField] AudioClip[] clips;
+
+	Transform playerLoc;
+
+	// Use this for initialization
+	void Start () {
+		aud = this.GetComponent<AudioSource> ();
+		playerLoc = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(playerLoc.position, this.transform.position);
+		aud.volume = 1 - Mathf.Clamp(dist, 0, 10)/10;
+	}
+
+	void SetDown(int i){
+		aud.clip = clips [i];
+		aud.Play();
+	}
+}
---
+++ assignments/GAME1012_A2_NakasakoRicardo/Assets/MyAssets/Scripts/PorcupineScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PorcupineScript : MonoBehaviour {
+	[SerializeField] AudioSource aud;
+	Animator anim;
+
+	Transform playerLoc;
+
+	void Start () {
+		aud = this.GetComponent<AudioSource> ();
+		anim = this.GetComponent<Animator> ();
+		playerLoc = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(playerLoc.position, this.transform.position);
+		aud.volume = 1 - Mathf.Clamp (dist, 0, 10) / 10;
+		Debug.Log (dist);
+		anim.SetFloat ("PlayerDistance",dist);
+	}
+
+	void Snikt(){
+		aud.Play();
+	}
+}

4d83476f1a0dc21e281f3976ae991fa28892fdd5
Ricardo Shimoda <rshimoda@gmail.com>
2018-04-22 14:23:22 -0400

    some stuff

--- assignments/GAME1012_A2_NakasakoRicardo/Assets/MyAssets/Scripts/PlayerData.cs
+++ assignments/GAME1012_A2_NakasakoRicardo/Assets/MyAssets/Scripts/PlayerData.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class PlayerData 
 {
+	public const int MAX_LIVES = 4;
+	public const int MAX_AMMO = 25;
+
 	private static PlayerData instance = null;
-	private int lives = 4;
-	private int ammo = 25;
+	private int lives = MAX_LIVES;
+	private int ammo = MAX_AMMO;
 
 	public static PlayerData Instance
 	{
@@ -38,4 +41,11 @@ public class PlayerData
 		}
 	}
 
+	public void MaxLives(){
+		lives = MAX_LIVES;
+	}
+
+	public void MaxAmmo(){
+		ammo = MAX_AMMO;
+	}
 }
